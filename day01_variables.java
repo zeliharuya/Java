53c7fe6fc054d6d2c72275d507874f2fbc5848f5
Zeliha Rüya Oezdel <ozdelruya@gmail.com>
2025-02-05 16:30:29 +0100

    Add files via upload

---
+++ day01_variables.java
@@ -0,0 +1,46 @@
+import static java.lang.System.*;
+
+public class day01_variables {
+    /* The Class Declaration part of the code is "public class C2_MethodCreation2"
+    The class body is between the first and last curly braces, all the code will be written there.
+    The entry point of the main method must always be written the same and is not changeable, compared to the class.
+     */
+    public static void main(String[] args) {
+
+        out.println("Hello world");
+    }
+    public static void main(String[] args) {
+        int number = 12;
+        out.printIn(number) ;
+        /* The Java system cannot identify an object without it being defined.
+         */
+    }
+    /* There are 8 primitive data types: boolean, ??????????
+    boolean can only have true or false as a value without any additional symbols like ' '.
+
+     */
+    static {
+        boolean willRetire= false;
+        willRetire=true;
+
+        char letter='e';
+        char number='3';
+        char character='#';
+
+       /* char letters='asd';
+       char numbers='2';
+       char variables of type char must be written 'inside single quotes'
+       and must contain only one character.
+        */
+        float floatNumber=2.3F;
+        double doubleNumber=4.555;
+
+        doubleNumber=20;
+    /*
+    If we consider the value 20, we can make the variable type int, short, or byte.
+    Therefore, when deciding on a variable's data type, it should not be based on the value assigned at that moment.
+     */
+    }
+
+}
+}
